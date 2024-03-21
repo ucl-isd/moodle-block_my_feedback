1ec5ba98810ab17740e375d524c51c830b0d7ec8
Matthias Opitz <m.opitz@ucl.ac.uk>
2024-03-21 17:15:02 +0000

    require at least Moodle 4.2, removed tests against version 4.1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023031400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023031400;            // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = '1.0';
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->requires  = 2019051100;        // Requires this Moodle version.
-$plugin->component = 'block_my_feedback'; // Full name of the plugin (used for diagnostics).
+$plugin->requires  = 2023042400;            // Requires at least Moodle version 4.2.
+$plugin->component = 'block_my_feedback';   // Full name of the plugin (used for diagnostics).
 
